8a852f58e6c8cbec96e89ff0f56a52e379c9a287
DIMITRIOS KALAITZIS <d.kalaitzis@yahoo.com>
2021-01-22 11:47:36 +0000

    Create vue app

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="login"> Login </button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String 
+  },
+  methods: {
+    async login() {
+      const data = await axios.post('http://localhost/5000/api/auth/login', {email:'test@mail.com', password:'123123123'});
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 
 const db = require('./models')
 // app.get('/', (req, res) => {
@@ -14,6 +17,8 @@ passportConfig;
 
 const routes = require("./routes");
 app.use(routes);
+
+app.options('*', cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}!`);
